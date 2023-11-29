5d0e768d947afbb62ba173f1ee3dce4974f15dce
lina2016 <linajamadar@gmail.com>
2023-11-28 18:01:29 -0800

    third commit

--- restaurant/views.py
+++ restaurant/views.py
@@ -89,27 +89,3 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     
-# class MenuViewSet(viewsets.ModelViewSet):
-#     queryset = Menu.objects.all()
-#     serializer_class = MenuSerializer
-    
-#     def get_permissions(self):
-#         if self.request.method == "GET":
-#             permission_classes = [IsAuthenticated]
-#         else:
-#             permission_classes = [IsAdmin | IsManager]
-#         return [permission() for permission in permission_classes]
-
-
-# class BookingViewSet(viewsets.ModelViewSet):
-#     permission_classes = [IsAuthenticated]
-#     queryset = Booking.objects.all()
-#     serializer_class = BookingSerializer
-    
-#     def get_permissions(self):
-#         if self.request.method in ["GET", "POST"]:
-#             permission_classes = [IsAuthenticated]
-#         else:
-#             permission_classes = [IsAdmin | IsManager]
-#         return [permission() for permission in permission_classes]
-
